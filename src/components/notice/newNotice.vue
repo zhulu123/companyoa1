03949e619332e53bbb579b07c7305e7bdf7b51a1
wumenyga <wumengya@aliyun.com>
2019-04-16 23:22:51 +0800

    0416晚

--- src/components/notice/newNotice.vue
+++ src/components/notice/newNotice.vue
@@ -21,7 +21,7 @@
                 </el-form-item>
                 <el-form-item label="接收人员">
                    <div class="peoList">
-                       <a class="choPeo"><i class="icon iconfont iconrenyuanzengjia"></i></a>
+                       <a class="choPeo" @click="dialogVisible = true"><i class="icon iconfont iconrenyuanzengjia"></i></a>
                    </div>
                 </el-form-item>
                 <el-form-item label="通知正文">
@@ -38,6 +38,19 @@
             </quill-editor>
             <div class="bts"><el-button type="primary">提交</el-button><el-button>取消</el-button></div>
         </div>
+
+        <el-dialog
+            title="提示"
+            :visible.sync="dialogVisible"
+            width="30%"
+            :before-close="handleClose">
+            <span>这是一段信息</span>
+            <span slot="footer" class="dialog-footer">
+                <el-button @click="dialogVisible = false">取 消</el-button>
+                <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+            </span>
+            </el-dialog>
+        
     </div>
 </template>
 
@@ -57,6 +70,7 @@ export default {
             content:'',
             editorOption:{},
             labelPosition:'right',
+            dialogVisible:false,
             form:{
                 title:'',
                 noticeType:'',
@@ -77,6 +91,13 @@ export default {
         },
         onSubmit(){
 
+        },
+        handleClose(){
+            this.$confirm('确认关闭？')
+            .then(_ => {
+                done();
+            })
+            .catch(_ => {});
         }
     }
 }
