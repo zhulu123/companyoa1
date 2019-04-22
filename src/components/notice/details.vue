d5a3d301721873742e599d349d6def4fbeca1199
wumengya1990 <wumengya@aliyun.com>
2019-04-22 17:55:37 +0800

    添加查看、删除内容

--- src/components/notice/details.vue
+++ src/components/notice/details.vue
@@ -19,8 +19,27 @@
                 
             </div>
             <div class="suspensionBts">
-                <el-tooltip class="item" effect="dark" content="分享转发" placement="right"><a><i class="el-icon-share"></i></a></el-tooltip>
-                <el-tooltip class="item" effect="dark" content="删除" placement="right"><a><i class="el-icon-delete"></i></a></el-tooltip>
+                <el-tooltip class="item" effect="dark" content="分享转发" placement="right"><a @click="shareBt"><i class="el-icon-share"></i></a></el-tooltip>
+                <el-badge :value="shuziL" class="item"><el-tooltip class="item" effect="dark" content="查阅情况" placement="right"><a  v-popover:popover1><i class="el-icon-view"></i></a></el-tooltip></el-badge>
+                <el-popover
+                    ref="popover1"
+                    placement="left-start"
+                    title="阅读情况详情"
+                    width="600"
+                    height="300"
+                    trigger="hover"
+                    >
+                    <div class="watchPeoList">
+                        <dl v-for="(n,index) in seeList" :key="index">
+                            <dt>{{n.department}}</dt>
+                            <dd>
+                                <span v-for="(m,indexn) in n.peoList" :class="{wtd:m.watched}" :key="indexn">{{m.peoName}}</span>
+                            </dd>
+                        </dl>
+                    </div>
+
+                </el-popover>
+                <el-tooltip class="item" effect="dark" content="删除" placement="right"><a @click="dtopThis"><i class="el-icon-delete"></i></a></el-tooltip>
                 <el-tooltip class="item" effect="dark" content="返回顶部" placement="right"><a><i class="el-icon-upload2"></i></a></el-tooltip>
             </div>
 
@@ -54,12 +73,11 @@
             </div>
         </div>
 
-        <div class="haveRead">
-            <h4></h4>
-            <div class="">
+        <el-dialog title="接收人员" append-to-body :visible.sync="shareBox">
+            <div class="shareBoxM">
                 
             </div>
-        </div>
+        </el-dialog>
 
     </div>
 </template>
@@ -69,6 +87,9 @@ export default {
     name:'ndetails',
     data() {
         return {
+            shuziL:'200/300',
+            shareBox:false,
+            activeNames: ['1'],
             scHeight:'',
             noticeCon:{
                 ntitle:'通知标题长长长长长长长长长长长长长长长长长长长长长长长长',
@@ -81,7 +102,46 @@ export default {
                     {ftitle:'',url:'',filetype:1}
                 ]
                 
-            }
+            },
+            seeList:[
+                {
+                    department:'校办公室',
+                    peoList:[
+                        {peoName:'张三',watched:false},
+                        {peoName:'李四',watched:false},
+                        {peoName:'王五',watched:true},
+                        {peoName:'苏商想',watched:false},
+                        {peoName:'石礅',watched:false}
+                    ]
+                },{
+                    department:'教务处',
+                    peoList:[
+                        {peoName:'张三',watched:false},
+                        {peoName:'李四',watched:false},
+                        {peoName:'王五王五',watched:true},
+                        {peoName:'苏商想',watched:true},
+                        {peoName:'石礅',watched:false}
+                    ]
+                },{
+                    department:'高一年级部',
+                    peoList:[
+                        {peoName:'张三',watched:false},
+                        {peoName:'李四',watched:false},
+                        {peoName:'王五',watched:false},
+                        {peoName:'苏商想',watched:false},
+                        {peoName:'石礅',watched:false}
+                    ]
+                },{
+                    department:'高二年级部',
+                    peoList:[
+                        {peoName:'张三',watched:false},
+                        {peoName:'李四',watched:false},
+                        {peoName:'王五',watched:false},
+                        {peoName:'苏商想',watched:false},
+                        {peoName:'石礅',watched:false}
+                    ]
+                }
+            ]
         }
     },
     mounted() {
@@ -94,6 +154,26 @@ export default {
           let scHeight = parseInt(thisheight)-280+"px";
           me.scHeight = scHeight;    
       },
+      dtopThis(){
+          this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          this.$message({
+            type: 'success',
+            message: '删除成功!'
+          });
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消删除'
+          });          
+        });
+      },
+      shareBt(){
+          this.shareBox = true;
+      }
     }
 }
 </script>
